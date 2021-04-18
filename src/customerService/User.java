c1102f78be610f2c567a4af8830a56ff2b94c2d9
PaSathees <sathpaskaran@gmail.com>
2021-04-18 13:14:49 +0530

    Added DB connect method for User for inter-service comm. with Project

--- src/customerService/User.java
+++ src/customerService/User.java
@@ -1,5 +1,29 @@
 package customerService;
 
-public class User {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class User {
+	public Connection connectForProjectManagement() {
+		Connection con = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");//not necessary as this way is deprecated
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:8889/GB_Customer", "root", 
+					"root");
+			System.out.println("Connection successfully established with Project DB");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
+	
+//	public boolean isAuthenticated(String username) {
+//		
+//	}
+//	
+//	public String getUserID(String username) {
+//		
+//	}
 }
